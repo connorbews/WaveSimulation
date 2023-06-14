f04d7ddf4830b5f66c25130ad01f0b6791bbb7dd
connorbews <connbews@telus.net>
2023-06-13 17:43:20 -0700

    rotation simulation

--- hello_triangle.cpp
+++ hello_triangle.cpp
@@ -12,23 +12,32 @@ namespace fs = std::filesystem;
 #include"include/EBO.h"
 #include"include/stb/stb_image.h"
 #include"include/Texture.h"
+#include"include/glm/glm.hpp"
+#include "include/glm/gtc/matrix_transform.hpp"
+#include "include/glm/gtc/type_ptr.hpp"
 
 
 
 // Vertices coordinates
 GLfloat vertices[] =
 {
-	-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, // Lower left corner
-	-0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, // Lower right corner
-	0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,// Upper corner
-	0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f// Inner left
+	-0.5f, 0.0f, 0.5f,  0.83f, 0.70f, 0.44f,  0.0f, 0.0f,
+	-0.5f, 0.0f, -0.5f, 0.83f, 0.70f, 0.44f,  5.0f, 0.0f,
+	 0.5f, 0.0f, -0.5f, 0.83f, 0.70f, 0.44f,  0.0f, 0.0f,
+	 0.5f, 0.0f, 0.5f,  0.83f, 0.70f, 0.44f,  5.0f, 0.0f,
+	 0.0f, 0.8f, 0.0f,  0.92f, 0.86f, 0.76f,  2.5f, 5.0f
+
 };
 
 // Indices for vertices order
 GLuint indices[] =
 {
-	0, 2, 1, // Lower left triangle
-	0, 3, 2, // Lower right triangle
+	0, 1, 2,
+	0, 2, 3,
+	0, 1, 4,
+	1, 2, 4,
+	2, 3, 4,
+	3, 0, 4,
 };
 
 
@@ -91,8 +100,6 @@ int main()
 	std::string parentDir = (fs::current_path().fs::path::parent_path()).string();
 	std::string texPath = "/glad/resources/";
 
-
-	std::cout << (parentDir + texPath + "miles.png").c_str() << std::endl;
 	// Texture
 	Texture popCat((parentDir + texPath + "miles.png").c_str(), GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
 	popCat.texUnit(shaderProgram, "tex0", 0);
@@ -101,15 +108,43 @@ int main()
 	/*Texture popCat("pop_cat.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
 	popCat.texUnit(shaderProgram, "tex0", 0);*/
 
+	float rotation = 0.0f;
+	double prevTime = glfwGetTime();
+
+	glEnable(GL_DEPTH_TEST);
+
 	// Main while loop
 	while (!glfwWindowShouldClose(window))
 	{
 		// Specify the color of the background
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		// Clean the back buffer and assign the new color to it
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		// Tell OpenGL which Shader Program we want to use
 		shaderProgram.Activate();
+
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 proj = glm::mat4(1.0f);
+
+		double crntTime = glfwGetTime();
+		if (crntTime - prevTime >= 1/60)
+		{
+			rotation += 0.5f;
+			prevTime = crntTime;
+		}
+		
+
+		model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
+		view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
+		proj = glm::perspective(glm::radians(45.0f), (float)(800 / 800), 0.1f, 100.0f);
+
+		int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
 
 		glUniform1f(uniID, 0.0f);
 
@@ -118,7 +153,7 @@ int main()
 		VAO1.Bind();
 		// Draw primpopCat.Delete();ers(window);
 
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, sizeof(indices)/ sizeof(int), GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 		// Take care of all GLFW events
